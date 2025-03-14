b369a2afcb155a7a3b0a43e2f803e19a509c2018
Tomer <57483589+tomer-w@users.noreply.github.com>
2025-03-14 02:22:06 +0200

    few more things to publish the package

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ setup(
     packages=find_packages(),
     version='0.1.0',
     description='NMEA 2000 encoder and decoder',
+    long_description=open('README.md').read(),  # Readme file for long description
+    long_description_content_type="text/markdown",
     author='Tomer-W',
+    url="https://github.com/tomer-w/nmea2000",
     install_requires=[],
     setup_requires=['pytest-runner'],
     tests_require=['pytest==8.3.5'],
