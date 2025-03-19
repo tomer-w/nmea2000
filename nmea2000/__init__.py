861f5b907093ed73cff138c1321753a12430a033
Tomer <57483589+tomer-w@users.noreply.github.com>
2025-03-19 17:01:31 +0200

    export the new consts

--- nmea2000/__init__.py
+++ nmea2000/__init__.py
@@ -1,6 +1,7 @@
 from .decoder import NMEA2000Decoder
 from .encoder import NMEA2000Encoder
-from .message import NMEA2000Message
+from .consts import FieldTypes, PhysicalQuantities
+from .message import NMEA2000Message, NMEA2000Field
 from .ioclient import TcpNmea2000Gateway, UsbNmea2000Gateway
 
-__all__ = ["NMEA2000Decoder", "NMEA2000Encoder", "NMEA2000Message", "NMEA2000Field", "TcpNmea2000Gateway", "UsbNmea2000Gateway"]
+__all__ = ["NMEA2000Decoder", "NMEA2000Encoder", "NMEA2000Message", "NMEA2000Field", "TcpNmea2000Gateway", "UsbNmea2000Gateway", "FieldTypes", "PhysicalQuantities"]
