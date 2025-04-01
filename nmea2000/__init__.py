1f8a508e9dd608b4c18a2f686f18a01830473d60
Tomer <57483589+tomer-w@users.noreply.github.com>
2025-04-01 23:18:52 +0300

    Minor change

--- nmea2000/__init__.py
+++ nmea2000/__init__.py
@@ -4,4 +4,4 @@ from .consts import FieldTypes, PhysicalQuantities
 from .message import NMEA2000Message, NMEA2000Field
 from .ioclient import TcpNmea2000Gateway, UsbNmea2000Gateway
 
-__all__ = ["NMEA2000Decoder", "NMEA2000Encoder", "NMEA2000Message", "NMEA2000Field", "TcpNmea2000Gateway", "UsbNmea2000Gateway", "FieldTypes", "PhysicalQuantities"]
+__all__ = ["NMEA2000Decoder", "NMEA2000Encoder", "NMEA2000Message", "NMEA2000Field", "TcpNmea2000Gateway", "UsbNmea2000Gateway", "AsyncIOClient", "FieldTypes", "PhysicalQuantities"]
